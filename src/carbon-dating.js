a949d74ad24b1885a4278501d7e79601864e9004
Aleksei Golubkov <nuriver@gmail.com>
2023-12-17 22:08:31 +0100

    feat: complete carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+
+  if (
+    isNaN(sampleActivity) ||
+    +sampleActivity > 15 ||
+    sampleActivity === null ||
+    typeof sampleActivity != "string" ||
+    +sampleActivity <= 0
+  ) {
+    return false;
+  } else {
+    let t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+    return t;
+  }
 }
 
 module.exports = {
