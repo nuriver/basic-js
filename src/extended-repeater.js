062227ddc57d2427f2d48dad85909ea5c40aa6fe
Aleksei Golubkov <nuriver@gmail.com>
2023-12-19 18:39:49 +0100

    feat: solve repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (typeof str != "string") {
+    str = str + "";
+  }
+
+  let defObj = {
+    repeatTimes: 0,
+    separator: "+",
+    addition: "",
+    additionRepeatTimes: 0,
+    additionSeparator: "|",
+  };
+  let additionStr = "";
+  let longStr = "";
+
+  Object.assign(defObj, options);
+  
+
+
+ if (typeof defObj.addition != "string") {
+    defObj.addition = defObj.addition + "";
+  }
+
+  if (defObj.repeatTimes == 0 && defObj.additionRepeatTimes == 0) {
+    longStr = str + defObj.addition;
+    return longStr;
+  }
+
+  if (defObj.addition.length > 0 && defObj.additionRepeatTimes > 0) {
+    additionStr = (defObj.addition + defObj.additionSeparator).repeat(
+      defObj.additionRepeatTimes
+    );
+    additionStr = additionStr.substring(
+      0,
+      additionStr.length - defObj.additionSeparator.length
+    );
+  } 
+
+  if (defObj.addition.length > 0 && defObj.additionRepeatTimes == 0) {
+    additionStr = defObj.addition;
+  }
+
+
+
+
+
+  if (additionStr.length > 0) {
+    longStr = (str + additionStr + defObj.separator).repeat(defObj.repeatTimes);
+    longStr = longStr.substring(0, longStr.length - defObj.separator.length);
+  } else {
+    longStr = (str + defObj.separator).repeat(defObj.repeatTimes);
+    longStr = longStr.substring(0, longStr.length - defObj.separator.length);
+  }
+  return longStr;
 }
+
 
 module.exports = {
   repeater
