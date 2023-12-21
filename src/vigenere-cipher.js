c830de16e54463ad869a7c7223bbb9e19c689b82
Aleksei Golubkov <nuriver@gmail.com>
2023-12-21 20:44:08 +0100

    feat: solve vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,87 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value) {
+    if (value === false) {
+      this.type = "reverse";
+    } else {
+      this.type = "direct";
+    }
+    (this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+      (this.regEx = /[.!@#$%^&*()_+-=0-9| ]/);
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    let arr = [];
+    let lettersCount = 0;
+
+
+      for (let i = 0; i < message.length; i++) {
+        let encryptedLetter =
+          this.alphabet[
+            this.alphabet.indexOf(message[i].toUpperCase()) +
+              this.alphabet.indexOf(`${key[lettersCount]}`.toUpperCase())
+          ];
+
+        if (this.regEx.test(message[i])) {
+          arr.push(message[i]);
+        } else {
+          arr.push(encryptedLetter);
+          lettersCount++;
+          if (lettersCount > key.length - 1) {
+            lettersCount = 0;
+          }
+        }
+    }
+    if (this.type === "direct") {
+       return arr.join("");
+    } else {
+      return arr.reverse().join("");
+     }
+  }
+
+  decrypt(message, key) {
+        if (message === undefined || key === undefined) {
+          throw new Error("Incorrect arguments!");
+        }
+    let arr = [];
+    let lettersCount = 0;
+
+      for (let i = 0; i < message.length; i++) {
+        let decryptedLetter =
+          this.alphabet[
+            this.alphabet.lastIndexOf(message[i].toUpperCase()) -
+              this.alphabet.indexOf(`${key[lettersCount]}`.toUpperCase())
+          ];
+
+        if (this.regEx.test(message[i])) {
+          arr.push(message[i]);
+        } else {
+          arr.push(decryptedLetter);
+          lettersCount++;
+          if (lettersCount > key.length - 1) {
+            lettersCount = 0;
+          }
+        }
+    }
+    if (this.type === "direct") {
+return arr.join("");
+    } else {
+      return arr.reverse().join("");
+    }
+      
+    
   }
 }
+
+const directMachine = new VigenereCipheringMachine();
+//let regEx = /[^A-za-z]/;
+console.log(directMachine.decrypt("HSVD AJAL ^^", "behappy"));
+
 
 module.exports = {
   VigenereCipheringMachine
